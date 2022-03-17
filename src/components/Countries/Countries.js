bc3b2271c8cbb1c0f9616a3e13f0859fb747c97f
Md-Omar-Shahariar <osafridi00@gmail.com>
2022-03-17 20:50:04 +0600

    rest countries

--- src/components/Countries/Countries.js
+++ src/components/Countries/Countries.js
@@ -1,8 +1,9 @@
 import React, { useEffect, useState } from "react";
 import Country from "../Country/Country";
+import "./Countries.css";
 
 const Countries = () => {
-  let [countries, setCountries] = useState([]);
+  const [countries, setCountries] = useState([]);
   useEffect(() => {
     fetch("https://restcountries.com/v3.1/all")
       .then((res) => res.json())
@@ -13,12 +14,11 @@ const Countries = () => {
     <div>
       <h1>Hello From Country: {countries.length}</h1>
 
-      {countries.map((country) => (
-        <Country
-          name={country.name.common}
-          population={country.population}
-        ></Country>
-      ))}
+      <div className="countries-container">
+        {countries.map((country) => (
+          <Country country={country} key={country.cca3}></Country>
+        ))}
+      </div>
     </div>
   );
 };
--- src/components/Country/Country.js
+++ src/components/Country/Country.js
@@ -2,10 +2,13 @@ import React from "react";
 import "./Country.css";
 
 const Country = (props) => {
+  const { name, population, flags } = props.country;
+
   return (
     <div className="country">
-      <h2>Country Name: {props.name}</h2>
-      <h4>Population: {props.population}</h4>
+      <h2>Country Name: {name.common}</h2>
+      <img src={flags.svg} alt="" />
+      <h4>Population: {population}</h4>
     </div>
   );
 };
